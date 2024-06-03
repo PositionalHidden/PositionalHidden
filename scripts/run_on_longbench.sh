16f46f96d895bdf47cd738dfd6c0b82965f8f380
yuyijiong <824182011@qq.com>
2024-06-03 17:01:32 +0800

    add data

--- scripts/run_on_longbench.sh
+++ scripts/run_on_longbench.sh
@@ -1,10 +1,10 @@
-CUDA_VISIBLE_DEVICES=0 python experiments//run_on_longbench.py \
+CUDA_VISIBLE_DEVICES=0 python experiments/run_on_longbench/run_on_longbench.py \
 --dataset_names "trec_e" "lcc_e" \
 --dataset_dir "./run_on_longbench/longbench" \
 --model_path "meta-llama/Llama-2-7b-chat-hf" \
 --scale-config "./configs/config_llama2_7b.json" 
 
-CUDA_VISIBLE_DEVICES=0 python run_on_longbench/run_on_longbench.py \
+CUDA_VISIBLE_DEVICES=0 python experiments/run_on_longbench/run_on_longbench.py \
 --dataset_type "longbench_en" \
 --dataset_dir "./run_on_longbench/longbench" \
 --model_path "meta-llama/Llama-2-7b-chat-hf" \
